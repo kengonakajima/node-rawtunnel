327681cf50d7752a0d46bd3e790c6e5dcc9aa35e
Kengo Nakajima <kengo.nakajima@gmail.com>
2015-05-29 16:01:44 +0900

    listen test

--- client.js
+++ client.js
@@ -3,6 +3,11 @@ var msgpack = require("msgpack");
 
 var controlPort = 7770;
 
+if( process.argv.length < 3 ) {
+    console.log( "Usage: node client.js HOSTNAME -R TUNNELPORT:TARGETIP:TARGETPORT" );
+    process.exit(1);
+}
+
 var options = {};
 options.host = process.argv[2];
 options.port = controlPort;
@@ -16,7 +21,14 @@ console.log( "packed length:", packed.length );
 
 conn.write(packed);
 
+conn.write( msgpack.pack( [ "tunnel", 60000 ] ) );
+    
 var ms = new msgpack.Stream(conn);
 ms.addListener( "msg", function(m) {
     console.log( "received message:", m );
 });
+
+
+setInterval( function() {
+    conn.write( msgpack.pack( [ "list" ] ) );
+}, 500 );
--- server.js
+++ server.js
@@ -3,12 +3,39 @@ var msgpack = require("msgpack");
 
 var controlPort = 7770;
 
+
+
+function Tunnel(sv,p) {
+    this.server = sv;
+    this.portnum = p;
+}
+
+//Tunnel.prototype.
+
+function addTunnel(portnum) {
+    var sv = net.createServer( function(conn) {
+        console.log("Connection for tunnel from " + conn.remoteAddress );
+    });
+    
+    sv.listen( portnum, "0.0.0.0" );
+
+    var tun = new Tunnel(sv,portnum);
+    tun.error = null;
+    
+    sv.on("error",function(e){
+        console.log("tun error:",e);
+        tun.error = e;
+    });
+    return tun;
+}
+
 // main server
 var server = net.createServer( function(conn) {
     console.log("Connection from " + conn.remoteAddress );
 
     // init
-    conn.authorized = false;    
+    conn.authorized = false;
+    conn.tunnels = [];    
     var o = [ "hello", "world" ];
     conn.write( msgpack.pack(o) );    
 
@@ -21,12 +48,32 @@ var server = net.createServer( function(conn) {
         if( cmd == "echo" ) {
             var rest = m.slice(1,1+m.length);
             conn.write( msgpack.pack(rest) );            
+        } else if( cmd == "tunnel" ) { // create a new tunnel port. arg=["add", PORTNUM] ret=["OK"] or ["ERROR"]
+            var portnum = m[1];
+            var tun = addTunnel( portnum );
+            console.log("new tunnel:", tun );
+            conn.tunnels.push(tun);
+        } else if( cmd == "list" ) {
+            var out = [];
+            conn.tunnels.forEach( function(tun) {
+                out.push( { "port" : tun.portnum, "error" : tun.error });
+            });
+            conn.write( msgpack.pack(out));
         }
+    });
+
+    conn.on("close", function() {
+        console.log( "Control conection closed: ", conn.remoteAddress );
+        conn.tunnels.forEach( function(tun) {
+            tun.server.close();
+        });
     });
 
     
 });
 
 server.listen( controlPort, "0.0.0.0" );
+
+
 
 console.log( "TCP server listening!" );
